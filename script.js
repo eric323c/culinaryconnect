f074a424989b6728170dd8b9a35bdce8e22c4c2a
eric323c <71191021+eric323c@users.noreply.github.com>
2025-01-03 04:22:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Selectors
+const heroSearchBar = document.getElementById("hero-search-bar");
+
+// Function to navigate to the search page
+function navigateToSearchPage() {
+    // Redirect to the search.html page
+    window.location.href = "search.html";
+}
+
+// Event listener for the hero search bar
+heroSearchBar.addEventListener("click", navigateToSearchPage);
 
